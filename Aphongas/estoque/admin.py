4ca9c0669bf1878fe8a5cc0012ce5bbf694f40a6
Gabriel220704 <gabrielfelipevieira2220@gmail.com>
2025-06-07 07:47:31 -0300

    Criação do superusuário e registro dos modelos no admin

--- Aphongas/estoque/admin.py
+++ Aphongas/estoque/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Deposito, Fornecedor, Produto, Movimentacao
 
-# Register your models here.
+admin.site.register(Deposito)
+admin.site.register(Fornecedor)
+admin.site.register(Produto)
+admin.site.register(Movimentacao)
